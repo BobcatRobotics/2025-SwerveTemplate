00f907690162286ac8357b628f154d3c9a0bc09b
aopalka <79727669+aopalka@users.noreply.github.com>
2025-04-01 21:15:44 -0400

    Depcrate Generated TunerConstants

--- Base/src/main/java/frc/robot/BuildConstants.java
+++ Base/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Base";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "a16904c02b97904b82c96b7a9b4bdc0aaea5115b";
-  public static final String GIT_DATE = "2025-04-01 20:43:54 EDT";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "32a43de7bbc394c9110b44fa2079f9a4224d4f71";
+  public static final String GIT_DATE = "2025-04-01 20:59:33 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-04-01 20:54:54 EDT";
-  public static final long BUILD_UNIX_TIME = 1743555294815L;
+  public static final String BUILD_DATE = "2025-04-01 21:12:07 EDT";
+  public static final long BUILD_UNIX_TIME = 1743556327241L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
